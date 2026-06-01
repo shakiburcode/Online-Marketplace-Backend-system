477b2f2cd9c5eb5ab18db7e1fc26a5a3dd88daf2
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2026-06-01 23:51:35 +0600

    Update migration file for cart and cart items

--- user-service/migrations/20240328040356-cart-n-cart-items.js
+++ user-service/migrations/20240328040356-cart-n-cart-items.js
@@ -11,6 +11,7 @@ var Promise;
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
+
 exports.setup = function(options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
