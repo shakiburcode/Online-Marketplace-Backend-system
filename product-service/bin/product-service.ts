d5cf414e71c314ff8efe915fc433f2ad04760e56
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2026-06-25 15:12:54 +0600

    Update product-service.ts

--- product-service/bin/product-service.ts
+++ product-service/bin/product-service.ts
@@ -2,6 +2,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { ProductServiceStack } from '../lib/product-service-stack';
 
-
 const app = new cdk.App();
 new ProductServiceStack(app, 'ProductServiceStack');
